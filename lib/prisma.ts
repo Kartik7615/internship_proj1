9c127a811e1af65aeafddea318f7b4d72ecfa4c4
Sujal-D-88 <24kothales@rbunagpur.in>
2026-03-06 14:07:44 +0530

    3rd Commit: for adding pagination and edit delete option

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -1,4 +1,4 @@
-import { PrismaClient } from '@/app/generated/prisma'
+import { PrismaClient } from '@prisma/client/extension'
 import { PrismaNeon } from '@prisma/adapter-neon'
 
 const prismaClientSingleton = () => {
